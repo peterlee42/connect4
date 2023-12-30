699450c16c8ced0a838381861f2f6a859ec3b6cb
Peter Lee <76502486+peterlee42@users.noreply.github.com>
2023-12-30 02:22:07 -0500

    Uploaded files

---
+++ TRUEconnect4_final/TRUEconnect4_final/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace TRUEconnect4_final
+{
+    partial class gameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbGame = new System.Windows.Forms.PictureBox();
+            this.gbScoreboard = new System.Windows.Forms.GroupBox();
+            this.txtRWin = new System.Windows.Forms.TextBox();
+            this.lblRedWin = new System.Windows.Forms.Label();
+            this.txtYWin = new System.Windows.Forms.TextBox();
+            this.lblYellowWin = new System.Windows.Forms.Label();
+            this.btnResetScore = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.txtTurn = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.Connect4Timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbGame)).BeginInit();
+            this.gbScoreboard.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbGame
+            // 
+            this.pbGame.Location = new System.Drawing.Point(34, 101);
+            this.pbGame.Name = "pbGame";
+            this.pbGame.Size = new System.Drawing.Size(703, 603);
+            this.pbGame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbGame.TabIndex = 0;
+            this.pbGame.TabStop = false;
+            this.pbGame.Paint += new System.Windows.Forms.PaintEventHandler(this.pbGame_Paint);
+            this.pbGame.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbGame_MouseClick);
+            // 
+            // gbScoreboard
+            // 
+            this.gbScoreboard.Controls.Add(this.txtRWin);
+            this.gbScoreboard.Controls.Add(this.lblRedWin);
+            this.gbScoreboard.Controls.Add(this.txtYWin);
+            this.gbScoreboard.Controls.Add(this.lblYellowWin);
+            this.gbScoreboard.Controls.Add(this.btnResetScore);
+            this.gbScoreboard.Font = new System.Drawing.Font("Showcard Gothic", 9F);
+            this.gbScoreboard.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.gbScoreboard.Location = new System.Drawing.Point(762, 143);
+            this.gbScoreboard.Name = "gbScoreboard";
+            this.gbScoreboard.Size = new System.Drawing.Size(142, 244);
+            this.gbScoreboard.TabIndex = 2;
+            this.gbScoreboard.TabStop = false;
+            this.gbScoreboard.Text = "Scoreboard";
+            // 
+            // txtRWin
+            // 
+            this.txtRWin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtRWin.ForeColor = System.Drawing.Color.White;
+            this.txtRWin.Location = new System.Drawing.Point(10, 112);
+            this.txtRWin.Multiline = true;
+            this.txtRWin.Name = "txtRWin";
+            this.txtRWin.ReadOnly = true;
+            this.txtRWin.Size = new System.Drawing.Size(119, 30);
+            this.txtRWin.TabIndex = 0;
+            this.txtRWin.TabStop = false;
+            // 
+            // lblRedWin
+            // 
+            this.lblRedWin.AutoSize = true;
+            this.lblRedWin.Location = new System.Drawing.Point(7, 92);
+            this.lblRedWin.Name = "lblRedWin";
+            this.lblRedWin.Size = new System.Drawing.Size(71, 15);
+            this.lblRedWin.TabIndex = 5;
+            this.lblRedWin.Text = "Red Wins:";
+            // 
+            // txtYWin
+            // 
+            this.txtYWin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtYWin.ForeColor = System.Drawing.Color.White;
+            this.txtYWin.Location = new System.Drawing.Point(10, 51);
+            this.txtYWin.Multiline = true;
+            this.txtYWin.Name = "txtYWin";
+            this.txtYWin.ReadOnly = true;
+            this.txtYWin.Size = new System.Drawing.Size(119, 30);
+            this.txtYWin.TabIndex = 0;
+            this.txtYWin.TabStop = false;
+            // 
+            // lblYellowWin
+            // 
+            this.lblYellowWin.AutoSize = true;
+            this.lblYellowWin.Location = new System.Drawing.Point(7, 31);
+            this.lblYellowWin.Name = "lblYellowWin";
+            this.lblYellowWin.Size = new System.Drawing.Size(95, 15);
+            this.lblYellowWin.TabIndex = 3;
+            this.lblYellowWin.Text = "Yellow Wins:";
+            // 
+            // btnResetScore
+            // 
+            this.btnResetScore.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.btnResetScore.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnResetScore.Font = new System.Drawing.Font("Showcard Gothic", 15F);
+            this.btnResetScore.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnResetScore.Location = new System.Drawing.Point(10, 152);
+            this.btnResetScore.Name = "btnResetScore";
+            this.btnResetScore.Size = new System.Drawing.Size(119, 77);
+            this.btnResetScore.TabIndex = 1;
+            this.btnResetScore.Text = "Reset Scores";
+            this.btnResetScore.UseVisualStyleBackColor = false;
+            this.btnResetScore.Click += new System.EventHandler(this.btnResetScore_Click);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.btnQuit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnQuit.Font = new System.Drawing.Font("Showcard Gothic", 15F);
+            this.btnQuit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnQuit.Location = new System.Drawing.Point(762, 616);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(142, 85);
+            this.btnQuit.TabIndex = 7;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = false;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // txtTurn
+            // 
+            this.txtTurn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtTurn.Font = new System.Drawing.Font("Showcard Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTurn.ForeColor = System.Drawing.Color.White;
+            this.txtTurn.Location = new System.Drawing.Point(762, 101);
+            this.txtTurn.Multiline = true;
+            this.txtTurn.Name = "txtTurn";
+            this.txtTurn.ReadOnly = true;
+            this.txtTurn.Size = new System.Drawing.Size(129, 30);
+            this.txtTurn.TabIndex = 0;
+            this.txtTurn.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Showcard Gothic", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.lblTitle.Location = new System.Drawing.Point(146, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(480, 83);
+            this.lblTitle.TabIndex = 5;
+            this.lblTitle.Text = "CONNECT 4 >:(";
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnNewGame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.btnNewGame.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnNewGame.Font = new System.Drawing.Font("Showcard Gothic", 15F);
+            this.btnNewGame.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnNewGame.Location = new System.Drawing.Point(762, 416);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(142, 170);
+            this.btnNewGame.TabIndex = 6;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // Connect4Timer
+            // 
+            this.Connect4Timer.Tick += new System.EventHandler(this.Connect4Timer_Tick);
+            // 
+            // gameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(952, 724);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtTurn);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.gbScoreboard);
+            this.Controls.Add(this.pbGame);
+            this.Name = "gameForm";
+            this.Text = "Angry Connect 4 >:(";
+            ((System.ComponentModel.ISupportInitialize)(this.pbGame)).EndInit();
+            this.gbScoreboard.ResumeLayout(false);
+            this.gbScoreboard.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbGame;
+        private System.Windows.Forms.GroupBox gbScoreboard;
+        private System.Windows.Forms.Button btnResetScore;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.TextBox txtTurn;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.TextBox txtRWin;
+        private System.Windows.Forms.Label lblRedWin;
+        private System.Windows.Forms.TextBox txtYWin;
+        private System.Windows.Forms.Label lblYellowWin;
+        private System.Windows.Forms.Timer Connect4Timer;
+    }
+}
+
---
+++ TRUEconnect4_final/TRUEconnect4_final/Form1.cs
@@ -0,0 +1,407 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace TRUEconnect4_final
+{
+    public partial class gameForm : Form
+    {
+        int[,] angryChipLocationArray = new int[7, 6];  //Array for chip locations
+        int turn;// global variable for turn
+        bool gameover = true;
+        int row, col; // row and col of peg that is being dropped in
+        int TempRow; // temporary row for animation
+        bool AnimationHappen; // bool to check if animation is happening
+
+
+        // Win numbers for scoreboard
+        int yellowWins = 0; 
+        int redWins = 0;
+
+        public gameForm()
+        {
+            InitializeComponent();
+            //Background of pbGame
+            pbGame.Image = Image.FromFile("elmo.gif");
+            //Initialize scoreboard
+            txtYWin.Text = "0";
+            txtRWin.Text = "0";
+        }
+
+        public void InitializeGame()
+        {
+            //initializing all array spaces to empty (0 = empty, 1 = occupied by yellow chip, 2 = occupied by red chip)
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    angryChipLocationArray[i, j] = 0;
+                }
+            }
+
+            turn = 1; // Yellow starts
+            gameover = false; //Game is not over
+        }
+
+
+        //initializing board
+        private void pbGame_Paint(object sender, PaintEventArgs e)
+        {
+            // Initialize graphics, x, y, i, j;
+            Graphics g = e.Graphics;
+            float x = 0, y = 0;
+            int i, j;
+
+            // Initialize pen
+            Pen borderPen = new Pen(Color.AntiqueWhite, 3);
+
+            //Veritcal lines
+            for (i = 0; i < 8; i++)
+            {
+                g.DrawLine(borderPen, x, 0, x, 600);
+                x = x + 100;
+            }
+            //Horizontal lines
+            for (j = 0; j < 7; j++)
+            {
+                g.DrawLine(borderPen, 0, y, 703, y);
+                y = y + 100;
+            }
+
+            //placing pegs
+            for (int p = 0; p < 7; p++)
+            {
+                for (int q = 0; q < 6; q++)
+                {
+                    if (angryChipLocationArray[p, q] ==1) //if yellow, place appropriate peg
+                    {
+                       
+
+                        g.DrawImage(Image.FromFile("Angry-Yellow-Peg.png"),p * 100, q * 100, 100, 100);
+                    }
+                    else if (angryChipLocationArray[p, q] == 2) //if red, place appropriate peg
+                    {
+                        g.DrawImage(Image.FromFile("Angry-Red-Peg.png"), p * 100, q * 100, 100, 100);
+                    }
+                }
+            }
+           
+        }
+        public int findRow(int col)
+        {
+            int row;
+            // Checks if row is empty and returns the row # if it is
+            for (int i = 5; i >= 0; i--)
+            {
+                if (angryChipLocationArray[col, i] == 0)
+                {
+                    row = i;
+                    return row;
+                }
+            }
+            return -1; // returns -1 if all are full
+        }
+
+        public bool HorizontalWin(int col, int row)
+        {
+            bool win = false; //to check if win
+            // scenario for xooo win
+            if (col<=3)
+            {
+                if (angryChipLocationArray[col, row] == angryChipLocationArray[col + 1, row] &&  angryChipLocationArray[col + 2, row] == angryChipLocationArray[col, row] && angryChipLocationArray[col + 3, row] == angryChipLocationArray[col, row]) //xooo
+                {
+                    win =true;
+                }
+               
+            }
+            // scenario for ooxo win
+            if (col >= 2 && col <= 5)
+            {
+
+                if (angryChipLocationArray[col, row] == angryChipLocationArray[col + 1, row] && angryChipLocationArray[col, row] == angryChipLocationArray[col - 2, row] && angryChipLocationArray[col, row] == angryChipLocationArray[col - 1, row]) //ooxo
+                {
+                    win = true;
+                }
+            }
+            // scenario for oxoo win
+            if (col>0 && col < 5)
+            {
+                if (angryChipLocationArray[col, row] == angryChipLocationArray[col + 1, row] && angryChipLocationArray[col, row] == angryChipLocationArray[col + 2, row] && angryChipLocationArray[col, row] == angryChipLocationArray[col - 1, row]) //oxoo
+                {
+                    win = true;
+                }
+            }
+            // scenario for ooox win
+            if (col > 2)
+            {
+                if (angryChipLocationArray[col, row] == angryChipLocationArray[col - 1, row] && angryChipLocationArray[col, row] == angryChipLocationArray[col - 2, row] && angryChipLocationArray[col, row] == angryChipLocationArray[col - 3, row]) //ooox
+                {
+                    win = true;
+                }
+            }
+            return win;
+        }
+
+        public bool VerticalWin(int col, int row)
+        {
+            bool win = false; //bool for win
+            if (row < 3) // scenario for vertical win
+            {
+                if (angryChipLocationArray[col, row] == angryChipLocationArray[col, row + 1] && angryChipLocationArray[col, row + 2] == angryChipLocationArray[col, row] && angryChipLocationArray[col, row + 3] == angryChipLocationArray[col, row]) //xooo
+                {
+                    win = true;
+                }
+
+            }
+            return win;
+        }
+
+        public bool DiagonalWin(int col, int row)
+        {
+            bool win = false;
+
+            //diagonal going up
+            if (col < 4 && row > 2) //scenario for xooo win
+            {
+                if (angryChipLocationArray[col, row] == angryChipLocationArray[col + 1, row - 1] && angryChipLocationArray[col, row] == angryChipLocationArray[col + 2, row - 2] && angryChipLocationArray[col, row] == angryChipLocationArray[col + 3, row - 3]) //xooo
+                {
+                    win = true;
+                }
+               
+            }
+            if (col < 5 && col > 0 && row < 5 && row > 1) //scenario for oxoo win
+            {
+                if (angryChipLocationArray[col, row] == angryChipLocationArray[col -1, row + 1] && angryChipLocationArray[col, row] == angryChipLocationArray[col + 1, row - 1] && angryChipLocationArray[col, row] == angryChipLocationArray[col + 2, row - 2])//oxoo
+                {
+                    win = true;
+                }
+            }
+            if (col > 1 && col < 6 && row > 0 && row < 4) //scenario for ooxo win
+            {
+                if (angryChipLocationArray[col, row] == angryChipLocationArray[col - 2, row + 2] && angryChipLocationArray[col, row] == angryChipLocationArray[col - 1, row + 1] && angryChipLocationArray[col, row] == angryChipLocationArray[col + 1, row - 1])//ooxo
+                {
+                    win = true;
+                }
+            }
+            if (col > 2 && row < 3) //scenario for ooox win
+            {
+                if (angryChipLocationArray[col, row] == angryChipLocationArray[col - 3, row + 3] && angryChipLocationArray[col, row] == angryChipLocationArray[col - 2, row + 2] && angryChipLocationArray[col, row] == angryChipLocationArray[col - 1, row + 1])//ooox
+                {
+                    win = true;
+                }
+            }
+
+            //diagonal going down
+            if (col < 4 && row < 3) //scenario for xooo win
+            {
+                if (angryChipLocationArray[col, row] == angryChipLocationArray[col + 1, row + 1] && angryChipLocationArray[col, row] == angryChipLocationArray[col + 2, row + 2] && angryChipLocationArray[col, row] == angryChipLocationArray[col + 3, row + 3]) //xooo
+                {
+                    win = true;
+                }
+            }
+            if (col > 0 && col < 5 && row > 0 && row < 4) //scenario for oxoo win
+            {
+                if (angryChipLocationArray[col, row] == angryChipLocationArray[col - 1, row - 1] && angryChipLocationArray[col, row] == angryChipLocationArray[col + 1, row + 1] && angryChipLocationArray[col, row] == angryChipLocationArray[col + 2, row + 2]) //oxoo
+                {
+                    win = true;
+                }
+            }
+            if (col > 1 && col < 6 && row > 1 && row < 5) //scenario for ooxo win
+            {
+                if (angryChipLocationArray[col, row] == angryChipLocationArray[col - 2, row - 2] && angryChipLocationArray[col, row] == angryChipLocationArray[col - 1, row - 1] && angryChipLocationArray[col, row] == angryChipLocationArray[col + 1, row + 1])//ooxo
+                {
+                    win = true;
+                }
+            }
+            if (col > 2 && row > 2) //scenario for ooox win
+            {
+                if (angryChipLocationArray[col, row] == angryChipLocationArray[col - 3, row - 3] && angryChipLocationArray[col, row] == angryChipLocationArray[col - 2, row - 2] && angryChipLocationArray[col, row] == angryChipLocationArray[col - 1, row - 1])//ooox
+                {
+                    win = true;
+                }
+            }
+            return win;
+
+        }
+
+        private void pbGame_MouseClick(object sender, MouseEventArgs e)
+        {
+            try {
+
+                // Doesn't let click if animation happening
+                if (AnimationHappen == true)
+                {
+                    return;
+                }
+
+                // Message if gameover
+                if(gameover==true)
+                {
+                    //error and return
+                    MessageBox.Show("Please start a new game :)");
+                    return;
+                }
+                int mouseX = e.X;
+
+                // Checks which column mouse has clicked
+                col = -1;
+                for (int i = 0; i < 7; i++)
+                {
+                    if (mouseX > i * 100 && mouseX < (i + 1) * 100)
+                    {
+                        col = i;
+                        break;
+                    }
+                }
+
+                //Finds empty row of column
+                row = findRow(col);
+
+                //Message if column full
+                if (row == -1)
+                {
+                    MessageBox.Show("This column is full!");
+                }
+                //Else, start timer for animation
+                else
+                {
+                    AnimationHappen = true;
+                    TempRow = 0;
+                    Connect4Timer.Start();
+                }
+            }
+            catch
+            {
+                //Error message
+                MessageBox.Show("ERROR");
+            }
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            // Starts new game
+            try
+            {
+                InitializeGame();
+                pbGame.Invalidate();
+                txtTurn.Text = "Yellows Turn";
+            }
+            catch
+            {
+                //Error message
+                MessageBox.Show("ERROR");
+            }
+        }
+       
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Quits application
+                MessageBox.Show("Thanks for playing!");
+                System.Windows.Forms.Application.ExitThread();
+            }
+            catch
+            {
+                //Error message
+                MessageBox.Show("ERROR");
+            }
+        }
+
+        private void btnResetScore_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Resets score and output it
+                yellowWins = 0;
+                redWins = 0;
+                txtYWin.Text = yellowWins.ToString();
+                txtRWin.Text = redWins.ToString();
+            }
+            catch
+            {
+                //Error message
+                MessageBox.Show("ERROR");
+            }
+        }
+
+        private void Connect4Timer_Tick(object sender, EventArgs e)
+        {
+
+            if (TempRow==row)
+            {
+                //stops timer
+                Connect4Timer.Stop();
+                //Removes peg before the real peg if row not 0
+                if(row != 0)
+                {
+                    angryChipLocationArray[col, row - 1] = 0;
+                }
+                // Puts peg into (col, row) of the chip array
+                angryChipLocationArray[col, row] = turn;
+                pbGame.Invalidate();
+
+                //win condition + changing turns
+                bool winH = HorizontalWin(col, row);
+                bool winV = VerticalWin(col, row);
+                bool winD = DiagonalWin(col, row);
+
+                // Animation false
+                AnimationHappen = false;
+
+                if (turn == 1) // yellow has turn
+                {
+                    if (winH == true || winV == true || winD == true) // Checks if any win-condition is true
+                    {
+                        //Outputs message, adds to score, updates score, makes game over, and returns.
+                        MessageBox.Show("Yellow Wins!!");
+                        yellowWins++;
+                        txtYWin.Text = yellowWins.ToString();
+                        txtRWin.Text = redWins.ToString();
+                        gameover = true;
+                        return;
+                    }
+                    turn = 2;  // give turn to Red
+                    txtTurn.Text = "Red's Turn";
+                }
+                else // Red has a turn
+                {
+                    if (winH == true || winV == true || winD == true) // Checks if any win-condition is true
+                    {
+                        //Outputs message, adds to score, updates score, makes game over, and returns.
+                        MessageBox.Show("Red  Wins!!");
+                        redWins++;
+                        txtYWin.Text = yellowWins.ToString();
+                        txtRWin.Text = redWins.ToString();
+                        gameover = true;
+                        return;
+                    }
+                    turn = 1;  // give turn to yellow
+                    txtTurn.Text = "Yellow's Turn";
+                }
+               
+            }
+            //else creates temp peg
+            else
+            {
+                if (TempRow != 0)
+                {
+                    //Empties row before temprow if temprow isn't 0
+                    angryChipLocationArray[col, TempRow - 1] = 0;
+                }
+                // Adds peg to chip array of col, temprow
+                angryChipLocationArray[col, TempRow] = turn;
+                //Add 1 to temprow
+                TempRow = TempRow + 1;
+                pbGame.Invalidate();
+            }
+        }
+    }
+}
